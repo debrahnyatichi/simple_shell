ff9fbf486cb89c822d69f2df6ca4c01fa4fb26b1
AbbaKTG <abbaktg876@gmail.com>
2023-04-26 14:05:39 -0700

    made some changes

--- simple_shell.h
+++ simple_shell.h
@@ -38,42 +38,12 @@ extern char **input_cmds;
 extern char *shell_name;
 extern int status;
 
-/*helper functions*/
-void print_to_stdout(char *string, int length);
-char **split_string(char *string, char *deli);
-void remove_trailing_newline(char *string);
-int string_length(char *string);
-void copy_ztring(char *destin, char *source);
-
-/*helper functions 2*/
-int compare_strings(char *string1, char *string2);
-char *concatenate_strings(char *string1, char *string2);
-int span_string(char *string, char *characs);
-int complement_span_string(char *string, char *characs);
-char *find_character_in_string(char *string, char *charac);
-
-/* Helper functions 3*/
-char *get_next_token(char *string, char *deli, char **save_pointer);
-int convert_to_integer(char *string);
-void *reallocate_memory(void *pointer, unsigned int old_size,
-		unsigned int new_size);
-void ctrl_c_handler(int signal);
-void remove_comment_from_line(char *line);
-
-/* Utility functions */
-int identify_command_type(char *comn);
-void execute_command(char **comn_args, int comn_type);
-char *search_path_for_command(char *comn_name);
-void (*get_command_function(char *comn_name))(char **arguments);
-char *get_environment_variable(char *variable_name);
-
-/* Built-in command functions */
-void show_environment_variables(char **arguments);
-void quit_shell(char **arguments);
-
-/* Main functions */
-extern void run_shell_non_interactively(void);
-extern void initialize_shell(char **current_comn, int comn_type);
+/*helper_functions*/
+void print_to_stdout(char *str, int stream);
+char **split_string(char *input, char *delim);
+void _rm_newline(char *str);
+int str_len(char *string);
+void str_cpy(char *src, char *dest);
 
 #endif
 
